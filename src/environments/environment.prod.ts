e2f9c00813d2969ba076af6821d2b94921f1da41
elciojunior7 <elciosouza.junior@gmail.com>
2022-04-04 22:47:54 -0300

    envs vars ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
 export const environment = {
-   production: false,
-   supabaseKey: process.env.NG_APP_SUPABASE_KEY,
-   supabaseUrl: process.env.NG_APP_SUPABASE_URL,
-   supabaseImagesUrl: process.env.NG_APP_SUPABASE_IMAGES_BASEURL
- };
+  production: true
+};
